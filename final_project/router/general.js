fdcf9157946e9a0833b2d2ba24d9d31083c1aca1
ravula7 <ravula7@ad.unc.edu>
2023-08-02 23:12:38 -0400

    tasks 1-4, 8

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,13 +17,15 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn])
+    const promise = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        res.send(books[isbn]);   
+        });   
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    let booksbyauthor = [];
+  let booksbyauthor = [];
   let isbns = Object.keys(books);
   isbns.forEach((isbn) => {
     if(books[isbn]["author"] === req.params.author) {
@@ -37,8 +39,16 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    res.send(books[title]);
+  let booksbytitle = [];
+  let isbns = Object.keys(books);
+  isbns.forEach((isbn) => {
+    if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+                          "author":books[isbn]["author"],
+                          "reviews":books[isbn]["reviews"]});
+    }
+  });
+  res.send(JSON.stringify({booksbytitle}, null, 4));
 });
 
 //  Get book review
